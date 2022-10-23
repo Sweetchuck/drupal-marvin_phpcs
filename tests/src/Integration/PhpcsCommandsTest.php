d3dcd5b909f32e14cde11b90dd33dbcda2002be4
Andor <david-andor@kozpontiagy.hu>
2022-10-23 12:31:47 +0200

    WIP - Basic implementation

---
+++ tests/src/Integration/PhpcsCommandsTest.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\marvin_phpcs\Integration;
+
+/**
+ * @group marvin
+ * @group drush-command
+ *
+ * @covers \Drush\Commands\marvin_phpcs\PhpcsCommandsBase<extended>
+ * @covers \Drupal\marvin\CommandDelegatorTrait
+ * @covers \Drupal\marvin_phpcs\Robo\PhpcsConfigFallbackTaskLoader
+ * @covers \Drupal\marvin_phpcs\Robo\Task\PhpcsConfigFallbackTask<extended>
+ */
+class PhpcsCommandsTest extends UnishIntegrationTestCase {
+
+  public function testMarvinLintPhpcsProject(): void {
+    $root = $this->getProjectRootDir();
+
+    $expected = [
+      'exitCode' => 2,
+      'stdError' => implode(PHP_EOL, [
+        '[Marvin - PHP_CodeSniffer config fallback] ',
+        " [PHP_CodeSniffer - lint files] runs cd '$root/tests/fixtures/project_01' && ../../../vendor/bin/phpcs --standard='Drupal,DrupalPractice' --report='json' -- 'docroot/modules/custom/' 'docroot/themes/custom/'",
+        ' [Sweetchuck\Robo\Phpcs\Task\PhpcsLintFiles]  PHP Code Sniffer found some errors :-( ',
+        ' [Sweetchuck\Robo\Phpcs\Task\PhpcsLintFiles]  Exit code 2',
+      ]),
+      'stdOutput' => 'Missing class doc comment',
+    ];
+
+    $this->drush(
+      'marvin:lint:phpcs',
+      ["$root/tests/fixtures/project_01"],
+      $this->getCommonCommandLineOptions(),
+      NULL,
+      NULL,
+      $expected['exitCode'],
+      NULL,
+      $this->getCommonCommandLineEnvVars()
+    );
+
+    $actualStdError = $this->getErrorOutput();
+    $actualStdOutput = $this->getOutput();
+
+    static::assertStringContainsString($expected['stdOutput'], $actualStdOutput, 'stdOutput');
+    static::assertSame($expected['stdError'], $actualStdError, 'stdError');
+  }
+
+  public function testMarvinLintPhpcsModule(): void {
+    $root = $this->getProjectRootDir();
+
+    $expected = [
+      'exitCode' => 2,
+      'stdError' => implode(PHP_EOL, [
+        '[Marvin - PHP_CodeSniffer config fallback] ',
+        " [PHP_CodeSniffer - lint files] runs cd '$root/tests/fixtures/project_01/docroot/modules/custom/dummy_m1' && ../../../../../../../vendor/bin/phpcs --standard='Drupal,DrupalPractice' --report='json' -- 'Commands/' 'tests/' 'dummy_m1.module'",
+        ' [Sweetchuck\Robo\Phpcs\Task\PhpcsLintFiles]  PHP Code Sniffer found some errors :-( ',
+        ' [Sweetchuck\Robo\Phpcs\Task\PhpcsLintFiles]  Exit code 2',
+      ]),
+      'stdOutput' => 'Missing class doc comment',
+    ];
+
+    $this->drush(
+      'marvin:lint:phpcs',
+      ["$root/tests/fixtures/project_01/docroot/modules/custom/dummy_m1"],
+      $this->getCommonCommandLineOptions(),
+      NULL,
+      NULL,
+      $expected['exitCode'],
+      NULL,
+      $this->getCommonCommandLineEnvVars()
+    );
+
+    $actualStdError = $this->getErrorOutput();
+    $actualStdOutput = $this->getOutput();
+
+    static::assertSame($expected['stdError'], $actualStdError, 'stdError');
+    static::assertStringContainsString($expected['stdOutput'], $actualStdOutput, 'stdOutput');
+  }
+
+}
